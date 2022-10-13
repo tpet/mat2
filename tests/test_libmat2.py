e936cda6c6585d1b49bca3ae7b3677ec527c699d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 15:04:48 +0000

    Adding tarfile member sanitization to extractall()

--- tests/test_libmat2.py
+++ tests/test_libmat2.py
@@ -600,7 +600,26 @@ class TestCleaningArchives(unittest.TestCase):
 
         tmp_dir = tempfile.mkdtemp()
         with tarfile.open('./tests/data/dirty.cleaned.tar') as zout:
-            zout.extractall(path=tmp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(zout, path=tmp_dir)
             zout.close()
 
         number_of_files = 0
@@ -635,7 +654,26 @@ class TestCleaningArchives(unittest.TestCase):
 
         tmp_dir = tempfile.mkdtemp()
         with tarfile.open('./tests/data/dirty.cleaned.tar.gz') as zout:
-            zout.extractall(path=tmp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(zout, path=tmp_dir)
             zout.close()
 
         number_of_files = 0
@@ -670,7 +708,26 @@ class TestCleaningArchives(unittest.TestCase):
 
         tmp_dir = tempfile.mkdtemp()
         with tarfile.open('./tests/data/dirty.cleaned.tar.bz2') as zout:
-            zout.extractall(path=tmp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(zout, path=tmp_dir)
             zout.close()
 
         number_of_files = 0
@@ -705,7 +762,26 @@ class TestCleaningArchives(unittest.TestCase):
 
         tmp_dir = tempfile.mkdtemp()
         with tarfile.open('./tests/data/dirty.cleaned.tar.xz') as zout:
-            zout.extractall(path=tmp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(zout, path=tmp_dir)
             zout.close()
 
         number_of_files = 0
